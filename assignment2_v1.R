7c0235c96bae960b5ddb32417daa56dd9a04af1c
hitsujioyaji <ddattilio@penumbra.inc>
2015-03-21 13:42:55 -0500

    ProgrammingAssignment2

---
+++ assignment2_v1.R
@@ -0,0 +1,22 @@
+makeCacheMatrix<-function(x=matrix()){
+        s<-NULL
+        set<-function(y) {
+                x<<-y
+                s<<-NULL
+        }
+        get<-function() x
+        setsolve<-function(solve) s <<-solve
+        getsolve<-function() s
+        list(set=set,get=get,setsolve=setsolve,getsolve=getsolve)
+}
+cacheSolve<-function(x,...){
+        s<-x$getsolve()
+        if(!is.null(s)){
+                message("getting cached data")
+                return(s)
+        }
+        matrix<-x$get()
+        s<-solve(matrix,...)
+        x$setsolve(s)
+        s
+}
